090221ca323db816a40fd109d08c5dd679247f27
vishnusangadala <70254663+vishnusangadala@users.noreply.github.com>
2021-11-29 16:24:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// getting all required elements
+
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
 const deleteAllBtn = document.querySelector(".footer button");
 
-// onkeyup event
+
 inputBox.onkeyup = ()=>{
   let userEnteredValue = inputBox.value; //getting user entered value
   if(userEnteredValue.trim() != 0){ //if the user value isn't only spaces
@@ -14,20 +14,19 @@ inputBox.onkeyup = ()=>{
   }
 }
 
-showTasks(); //calling showTask function
+showTasks(); 
 
-addBtn.onclick = ()=>{ //when user click on plus icon button
-  let userEnteredValue = inputBox.value; //getting input field value
-  let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
-  if(getLocalStorageData == null){ //if localstorage has no data
-    listArray = []; //create a blank array
+addBtn.onclick = ()=>{ 
+  let userEnteredValue = inputBox.value; 
+  let getLocalStorageData = localStorage.getItem("New Todo"); 
+  if(getLocalStorageData == null){//create a blank array
   }else{
-    listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
+    listArray = JSON.parse(getLocalStorageData);  
   }
-  listArray.push(userEnteredValue); //pushing or adding new value in array
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); //transforming js object into a json string
-  showTasks(); //calling showTask function
-  addBtn.classList.remove("active"); //unactive the add button once the task added
+  listArray.push(userEnteredValue); 
+  localStorage.setItem("New Todo", JSON.stringify(listArray));
+  showTasks(); 
+  addBtn.classList.remove("active");
 }
 
 function showTasks(){
@@ -38,21 +37,21 @@ function showTasks(){
     listArray = JSON.parse(getLocalStorageData); 
   }
   const pendingTasksNumb = document.querySelector(".pendingTasks");
-  pendingTasksNumb.textContent = listArray.length; //passing the array length in pendingtask
-  if(listArray.length > 0){ //if array length is greater than 0
-    deleteAllBtn.classList.add("active"); //active the delete button
+  pendingTasksNumb.textContent = listArray.length;
+  if(listArray.length > 0){ 
+    deleteAllBtn.classList.add("active");
   }else{
-    deleteAllBtn.classList.remove("active"); //unactive the delete button
+    deleteAllBtn.classList.remove("active"); 
   }
   let newLiTag = "";
   listArray.forEach((element, index) => {
     newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
   });
-  todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
-  inputBox.value = ""; //once task added leave the input field blank
+  todoList.innerHTML = newLiTag; 
+  inputBox.value = ""; 
 }
 
-// delete task function
+
 function deleteTask(index){
   let getLocalStorageData = localStorage.getItem("New Todo");
   listArray = JSON.parse(getLocalStorageData);
@@ -61,15 +60,14 @@ function deleteTask(index){
   showTasks(); //call the showTasks function
 }
 
-// delete all tasks function
 deleteAllBtn.onclick = ()=>{
-  let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
-  if(getLocalStorageData == null){ //if localstorage has no data
-    listArray = []; //create a blank array
+  let getLocalStorageData = localStorage.getItem("New Todo"); 
+  if(getLocalStorageData == null){ 
+    listArray = []; 
   }else{
-    listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
-    listArray = []; //create a blank array
+    listArray = JSON.parse(getLocalStorageData);  
+    listArray = []; 
   }
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); //set the item in localstorage
-  showTasks(); //call the showTasks function
+  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
+  showTasks(); 
 }
